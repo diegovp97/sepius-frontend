ea0cddcbf274c3371e5999c32dc6079c53b89d43
diegovp97 <diegovp976@gmail.com>
2026-06-10 11:58:14 +0200

    dw

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -3,14 +3,19 @@ const path = require('path');
 
 const apiUrl = process.env['API_URL'] || 'http://localhost:5000';
 
-const content = `export const environment = {
-  production: true,
-  apiUrl: '${apiUrl}'
-};
-`;
-
 const dir = path.join(__dirname, '..', 'src', 'environments');
 if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
 
-fs.writeFileSync(path.join(dir, 'environment.prod.ts'), content);
+fs.writeFileSync(path.join(dir, 'environment.ts'), `export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5000'
+};
+`);
+
+fs.writeFileSync(path.join(dir, 'environment.prod.ts'), `export const environment = {
+  production: true,
+  apiUrl: '${apiUrl}'
+};
+`);
+
 console.log(`environment.prod.ts generado con apiUrl: ${apiUrl}`);
